9800376af7c7ed2c948d0cf8c0b9968c469ed829
AhmadReza Zahedi <r.zahedi@gmail.com>
2019-08-19 14:31:04 +0430

    end-of-work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
+import { Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: []
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts
+        }))
+      })
+  }
+
+  removeContact = (contact) => {
+    this.setState( (currentState) => ({
+      contacts: currentState.contacts.filter((c)=>{
+        return c.id !== contact.id
+      })
+    }) )
+
+    ContactsAPI.remove(contact)
+  }
+
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then((contact) => {
+        this.setState((currentState) => ({
+          contacts: currentState.contacts.concat([contact])
+        }))
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Route exact path='/' render={()=>(
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+          />
+        )} />
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact)=> {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )} />
+      </div>
+    );
+  }
 }
 
 export default App;
